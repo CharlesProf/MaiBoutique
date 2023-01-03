18564f342d0df12acb558f111d23c5c6d75c8f09
CharlesProf <85425574+CharlesProf@users.noreply.github.com>
2023-01-03 15:57:21 +0200

    Update home.blade.php
    gw liat di inet dia pake cara java script buat dia munculin datanya di cart.web gitu jadi gw ikutin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,12 +18,19 @@
                                 {{-- Show product detail --}}
                                 <h5><strong>Product Detail</strong></h5>
                                 <p>{{$product->description}}</p>
-                                <p><strong>Quantity:</strong></p>
 
                                 {{-- Kolom input qty disini --}}
+                                <form action="/home" method="POST" enctype="multipart/form-data">
+                                @csrf
+                                <label for="Quantity"><b>Quantity</b></label>
+                                <div class="d-flex flex-row">
+                                    <input type="number" class="form-control" id="Quantity" aria-describedby="quantityHelp" placeholder="Enter Quantity">
+                                    {{-- Tombol add to cart disini--}}
+                                <button type="button" class="btn btn-block btn-success"  data-id="{{$product->id}}" data-name="{{$product->name}}" data-price="{{$product->price}}" data-description='{{$product->description}}'>Add</button>
+                                </div>
+                                </form>
 
-                                {{-- Tombol add to cart disini--}}
-
+                                <br>
                                 <a href="/home" style="margin-top: 10px" class=" mt-auto btn btn-danger ">Back</a>
                             @else
                                 {{-- Show all products --}}
@@ -42,3 +49,36 @@
         </div>
     </div>
 @endsection
+
+{{-- @section('footer-scripts')
+<script>
+    $(document).ready(function() {
+        window.cart = <?php echo json_encode($cart) ?>;
+        updateCartButton();
+        $('.add-to-cart').on('click', function(event){
+            var cart = window.cart || [];
+            cart.push({'id':$(this)data('id'), 'name':$(this).data('name'), 'price':$(this).data('price'), 'qty':$(this).prev('input').val()});
+            window.cart = cart;
+
+            $.ajax('/store/add-to-cart', {
+                type: 'POST',
+                data: {"_token": "{{ csrf_token() }}", "cart":cart},
+                success: function (data, status, xhr) {
+                }
+            });
+            updateCartButton();
+        });
+    })
+
+    function updateCartButton() {
+
+        var count = 0;
+        window.cart.forEach(function (item, i) {
+
+            count += Number(item.qty);
+        });
+
+        $('#items-in-cart').html(count);
+    }
+</script>
+@endsection --}}
